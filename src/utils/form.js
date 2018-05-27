18b8ecd53f0841c8f20052fbdfbce183c948d84a
Stevenson Marquez <stevensonxmarquez@gmail.com>
2018-05-27 12:56:38 -0500

    Ignorar eventos on- de js

--- src/utils/form.js
+++ src/utils/form.js
@@ -29,11 +29,15 @@ export function selectForm(event) {
 
 // Tipos de input a ser calificados
 const TYPES = [
-    "text", "password", "email", "number", "tel", "url"
+    "text", "password", "email", "number", "tel", "url", "search"
 ]
 
 // Atributos a ser ignorados
 const IGNORE_ATTRIBUTES = ["class", "style"]
+
+// Atributos
+const ATTRIBUTES = ['id', 'name', 'type', 'min', 'max', 
+    'maxlength', 'minlength', 'pattern', 'required', 'step']
 
 /**
  * Extrae los inputs del formulario en formato
@@ -44,13 +48,20 @@ export function getFormInputs(form) {
     let inputs = form.querySelectorAll('input');
     return Array.from(inputs)
         .filter(elem => TYPES.indexOf(elem.type) >= 0) // Filtra los tipos de input
-        .map(elem => {
-            let input = {};
-            for (let attr of elem.attributes) {
-                if(IGNORE_ATTRIBUTES.indexOf(attr.name) == -1) { // Filtra los attributos a ignorar
-                    input[attr.name] = attr.value // Agrega el atributo al objeto input
-                }
-            }
-            return input;
-        });
+        .map(elem => getInputAttributes(elem));
+}
+
+/**
+ * Obtiene un objeto con las reglas (atributos validos) del input
+ * @param {HTMLInputElement} elem
+ * @returns {Object} 
+ */
+export function getInputAttributes(elem) {
+    let input = {};
+    for (let attr of elem.attributes) {
+        if(ATTRIBUTES.indexOf(attr.name.toLowerCase()) > -1) { // Filtra los attributos a ignorar
+            input[attr.name] = attr.value // Agrega el atributo al objeto input
+        }
+    }
+    return input;
 }
